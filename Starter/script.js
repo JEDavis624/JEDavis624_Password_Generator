d7032def792b0bf73c390f537728653f4bca0f53
John Davis <johnedavis624@gmail.com>
2020-01-14 23:29:28 -0600

    got it to generate random password based on user input into the prompt.

--- Starter/script.js
+++ Starter/script.js
@@ -1,13 +1,12 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var maxLength = 128
-var minLength = 8
+
 
 const key_strings = {
 	lowercase: 'abcdefghijklmnopqrstuvwxyz',
 	uppercase: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
 	number: '0123456789',
-	symbol: '*;<>()[]{}#$?!^|'
+	specialCharacters: '*;<>()[]{}#$?!^|'
 };
 
 
@@ -15,9 +14,19 @@ const key_strings = {
 function writePassword() { 
  var length = prompt("Select between 8 and 128 characters");
  var specialCharacters = confirm("Special characters in your password?");
- var numeric = confirm("Numbers in your password?");
+ var number = confirm("Numbers in your password?");
  var lowercase = confirm("Lowercase letters in your password?");
  var uppercase = confirm("Uppercase letters in your password?");
+
+ function generatePassword() {
+  var length = 8,
+      charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
+      retVal = "";
+  for (var i = 0, n = charset.length; i < length; ++i) {
+      retVal += charset.charAt(Math.floor(Math.random() * n));
+  }
+  return retVal;
+}
 
 
 
